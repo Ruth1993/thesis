62e02771d38f1fe9de83af79f73b2cea0b926933
Ruth1993 <ruth.scholten@gmail.com>
2019-07-03 10:15:00 -0400

    removed some redundant lines from commitment_example2.cpp

--- commitment_example2.cpp
+++ commitment_example2.cpp
@@ -1,39 +1,29 @@
 
-Skip to content
-Pull requests
-Issues
-Marketplace
-Explore
-@Ruth1993
-Code Issues 2 Pull requests 0 Projects 0 Wiki Security Pulse Community
-libscapi/examples/Commitment/CommitmentExample.cpp
-@moriyaw moriyaw Rename "samples" package to "examples". d239324 on 25 Nov 2018
-172 lines (157 sloc) 7.75 KB
 /**
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-* 
+*
 * Copyright (c) 2016 LIBSCAPI (http://crypto.biu.ac.il/SCAPI)
 * This file is part of the SCAPI project.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-* 
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+* to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-* 
+*
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-* 
+*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-* 
+*
 * We request that any publication and/or code referring to and/or based on SCAPI contain an appropriate citation to SCAPI, including a reference to
 * http://crypto.biu.ac.il/SCAPI.
-* 
+*
 * Libscapi uses several open source libraries. Please see these projects for any further licensing issues.
 * For more information , See https://github.com/cryptobiu/libscapi/blob/master/LICENSE.MD
 *
 * %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-* 
+*
 */
 
 
@@ -133,7 +123,7 @@ int mainCommitment(string side, string configPath) {
 		make_shared<CommPartyTCPSynced>(io_service, committerParty, receiverParty) :
 		make_shared<CommPartyTCPSynced>(io_service, receiverParty, committerParty);
 	boost::thread t(boost::bind(&boost::asio::io_service::run, &io_service));
-	
+
 	try {
 		if (side == "1") {
 			server->join(500, 5000); // sleep time=500, timeout = 5000 (ms);
